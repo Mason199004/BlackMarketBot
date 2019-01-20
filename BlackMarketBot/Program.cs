233621b1990cee695dafb98cb22eb7e24c20ef08
Mason199004 <mason1990haines@gmail.com>
2019-01-20 15:22:24 -0600

    basic shit

--- BlackMarketBot/Program.cs
+++ BlackMarketBot/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace BlackMarketBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+
+            string token = File.ReadAllText("token.txt"); 
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
+
